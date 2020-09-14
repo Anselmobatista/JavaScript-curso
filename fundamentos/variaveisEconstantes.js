09ea534ddfeb1e9166e842bc6fe2c33e9ead0536
Anselmo Batista <67919142+Anselmobatista@users.noreply.github.com>
2020-09-14 15:18:29 -0300

    Bloco fundamentos em JS

---
+++ fundamentos/variaveisEconstantes.js
@@ -0,0 +1,16 @@
+var a = 3 //variavel
+let b = 4 //variavel
+
+var a = 30
+b = 40 //variavel
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 //valor fixo
+    //c = 50
+console.log(c)
